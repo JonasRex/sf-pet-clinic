a89f45a2f6369af9e0e9a7b7ab4614834d798df1
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-01-17 15:58:08 +0100

    Refactoring to common crud interface. closes #11.

--- pet-clinic-data/src/main/java/rex/springframework/sfpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/rex/springframework/sfpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package rex.springframework.sfpetclinic.services;
 
 import rex.springframework.sfpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
